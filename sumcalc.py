3053a9ef18c3f3fe4a644937a7ac39af588cb2ac
sona <sonamphutane23@gmail.com>
2026-04-21 23:43:26 +0530

    Comments added

--- sumcalc.py
+++ sumcalc.py
@@ -1,7 +1,8 @@
-def Sum(a,b,c,d):
+#function definition
+def Sum(a,b,c,d):# Function Parameters
     sum=a+b+c+d
     print(sum)
     return Sum
-Sum(1,2,3,4)
+Sum(1,2,3,4) #function Call Arguments
 
 Sum(3,6,7,8)
